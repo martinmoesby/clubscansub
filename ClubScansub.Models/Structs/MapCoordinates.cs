83b94f2cebb62e53ec627c6e7a32993a7d596491
WORKSTATION\Martin <martinmoesby@martinmoesby.com>
2019-11-16 10:16:59 +0100

    Implemented Route calculations on course sessions, that are not Academic.

---
+++ ClubScansub.Models/Structs/MapCoordinates.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClubScansub.Models.Structs
+{
+    public struct MapCoordinates
+    {
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+
+    }
+
+    public struct RoutePoints
+    {
+        public string StartAddress {get; set;}
+        public string EndAddress { get; set; }
+
+        public MapCoordinates Start { get; set; }
+        public MapCoordinates End { get; set; }
+    }
+
+}
--- ClubScansub/Areas/Members/Controllers/EventController.cs
+++ ClubScansub/Areas/Members/Controllers/EventController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using ClubScansub.Data;
 using ClubScansub.Extensions;
@@ -22,6 +23,9 @@ namespace ClubScansub.Areas.Members.Controllers
 
         }
 
+        [TempData]
+        public string StatusMessage { get; set; }
+
         [BindProperty]
         public IEnumerable<Event> Events { get; set; }
 
@@ -32,7 +36,7 @@ namespace ClubScansub.Areas.Members.Controllers
         public async Task<IActionResult> Index()
         {
             Events = await db.EventUsers.Include(x=>x.Event)
-                .Where(x=> !x.Event.IsCancelled && x.ApplicationUserId == User.GetIdentityId())
+                .Where(x=> !x.Event.IsCancelled && x.ApplicationUserId == User.GetIdentityId() && x.Event.EventType != EventTypeEnum.NotAnEvent)
                 .Select(x=>x.Event).Include(x=>x.Participants)
                 .OrderBy(x=>x.StartDateAndTime).ThenBy(x=>x.Id)
                 .ToListAsync();
@@ -59,6 +63,74 @@ namespace ClubScansub.Areas.Members.Controllers
                 .ToListAsync();
 
             return View(Courses);
+        }
+
+        public async Task<IActionResult> GetRoute(int CourseSessionId)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://dev.virtualearth.net");
+
+            var endPoint = await db.CourseSessions
+                .Include(x => x.CourseSessionTemplate).ThenInclude(x => x.Address)
+                .Include(x=>x.Divelocation).ThenInclude(x=>x.MeetingLocation)
+                .Include(x=>x.Address)
+                .FirstOrDefaultAsync(x => x.Id == CourseSessionId);
+
+            var endLocation = new Models.Structs.MapCoordinates();
+
+            var endPointAddress = endPoint.Address;
+            var endpointDiveLocationAdress = endPoint.Divelocation?.MeetingLocation;
+            var endpointCourseSessionTemplateAddress = endPoint.CourseSessionTemplate?.Address;
+            var endAddress = "";
+
+            if (endPointAddress == null)
+            {
+                if (endpointDiveLocationAdress== null)
+                {
+                    if (endpointCourseSessionTemplateAddress == null)
+                    {
+                        StatusMessage = "Ingen adresse på mødested/Dykkersted";
+                        return RedirectToAction(nameof(Index));
+                    } else
+                    {
+                        endLocation.latitude = endpointCourseSessionTemplateAddress.Latitude;
+                        endLocation.longitude = endpointCourseSessionTemplateAddress.Longitude;
+                        endAddress = $"{endpointCourseSessionTemplateAddress.Streetname},{endpointCourseSessionTemplateAddress.City},{endpointCourseSessionTemplateAddress.Country}";
+                    }
+                }
+                else
+                {
+                    endLocation.latitude = endpointDiveLocationAdress.Latitude;
+                    endLocation.longitude = endpointDiveLocationAdress.Longitude;
+                    endAddress = $"{endpointDiveLocationAdress.Streetname},{endpointDiveLocationAdress.City},{endpointDiveLocationAdress.Country}";
+                }
+            } else
+            {
+                endLocation.latitude = endPointAddress.Latitude;
+                endLocation.longitude = endPointAddress.Longitude;
+                endAddress = $"{endPointAddress.Streetname},{endPointAddress.City},{endPointAddress.Country}";
+            }
+
+
+            var user = await db.ApplicationUsers.FindAsync(User.GetIdentityId());
+            if (string.IsNullOrEmpty(user.Streetaddress) && string.IsNullOrEmpty(user.City) && string.IsNullOrEmpty(user.Country))
+            {
+                StatusMessage = "Du manlger at angive din adresse, før vi kan lave en rutebeskrivlese";
+                return LocalRedirect("/Identity/Account/Manage");
+            }
+
+            var startAddress = $"{user.Streetaddress},{user.City},{user.Country}";
+
+            var model = new Models.Structs.RoutePoints()
+            {
+                End = endLocation,
+                StartAddress = startAddress,
+                EndAddress = endAddress
+            };
+
+            return View(model);
+
+
         }
     }
 }
