5fc33e13d6b53fca2fb6f3bd8cd033bd7a1673a3
Martin Moesby Petersen <martinmoesby@martinmoesby.com>
2024-10-08 22:29:46 +0200

    fixed issues with WOrk completed. Added filtering fixed error that loaded sessions whichs had not been held

--- ClubScansub.Service/CourseService.cs
+++ ClubScansub.Service/CourseService.cs
@@ -72,13 +72,15 @@ namespace ClubScansub.Service
 
         public async Task<IList<CourseSessionInstructor>> GetCourseSessionInstructorAsync()
         {
-            var data = context.CourseSessionInstructors
+            var data = await context.CourseSessionInstructors
                 .Include(x => x.CourseSession)
-                .ThenInclude(x=>x.Course)
-                .Include(x=>x.Instructor)
-                .Where(x => x.CourseSession.DateTime < DateTime.UtcNow && x.InstructorApproved == true)
-                .OrderBy(x=>x.CourseSession.Course.StartDateAndTime).ThenBy(x=>x.CourseSession.DateTime).ThenBy(x =>x.Instructor.UserName);
-            return await data.ToListAsync();
+                .ThenInclude(x => x.Course).ThenInclude(x => x.Participants)
+                .Include(x => x.Instructor)
+                //.OrderBy(x => x.CourseSession.Course.StartDateAndTime).ThenBy(x => x.CourseSession.DateTime).ThenBy(x => x.Instructor.UserName)
+                .ToListAsync();
+
+            var result = data.Where(x => x.CourseSession.DateTime < DateTime.UtcNow && x.InstructorApproved == true && (x.CourseSession.Course.Participants != null || x.CourseSession.Course.FixedParticipants > 0)).ToList();
+            return result;
         }
 
         public async Task<Course> GetAsync(string Id)
