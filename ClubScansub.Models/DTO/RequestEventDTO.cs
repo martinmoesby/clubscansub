b5ce78ce3c0e0fd5f69f2e03025e668edaf6a689
Martin Moesby Petersen <martinmoesby@martinmoesby.com>
2025-11-23 11:17:03 +0100

    Begin - RequestEvent compoenet logic

---
+++ ClubScansub.Models/DTO/RequestEventDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubScansub.Models.DTO
+{
+    public class RequestEventDTO
+    {
+        public int DivelocationId { get; set; }
+        public DateOnly[] EventDates { get; set; }
+        public string[] Users { get; set; }
+
+        public RequestEventDTO() { }
+
+        public RequestEventDTO(int divelocationId, DateOnly[] eventDates, string[] users)
+        {
+            DivelocationId = divelocationId;
+            EventDates = eventDates;
+            Users = users;
+        }
+
+    }
+}
